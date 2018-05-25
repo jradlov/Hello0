2538905158f408b9e44403d80ef8e08f10836e16
Jaime Radulovich <jradlov@gmail.com>
2018-05-25 09:19:29 -0700

    just testing checking in a dummy project

---
+++ Hello0/Hello0/GreetPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hello0
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GreetPage : ContentPage
+	{
+		public GreetPage ()
+		{
+			InitializeComponent ();
+
+            mySlider.Value = 0.5;
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Title", "hello Steph!", "Ok");
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            // e.NewValue = slider position
+            myLabel.Text = String.Format("slider postion: {0:F2}", e.NewValue);
+        }
+    }
+}
